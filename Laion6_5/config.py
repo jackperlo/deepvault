dd281ce6ca9afa0bdea2ff01a9a05951e5e9222d
Giacomo <perlogiacomo@gmail.com>
2025-06-18 14:41:00 +0000

    Laion6.5+ dataset support added

---
+++ Laion6_5/config.py
@@ -0,0 +1,13 @@
+# !NOTE: constants MUST be in uppercase to be recognized by the codebase
+
+# Laion Hugging Face repository details
+REPO_ID = "jack-perlo/Laion6_5_plus_115k"
+FILENAME = "Laion6_5_plus_115k.tar.gz"
+
+LOCAL_DIR = "./datasets_data/laion6_5_compressed"
+IMAGES_DIR_TAR = './datasets_data/laion6_5_compressed/Laion6_5_plus_115k.tar.gz'
+
+DS_DIR = "./datasets_data/"
+IMAGES_DIR = "./datasets_data/laion6_5_images"
+CAPTIONS_CSV = "./datasets_data/laion6_5_captions"
+EMBEDDINGS_CSV = "./datasets_data/laion6_5_embeddings"
---
+++ Laion6_5/laion6_5_preprocessing.py
@@ -0,0 +1,42 @@
+import os
+from huggingface_hub import hf_hub_download
+import tarfile
+import shutil
+
+from config import REPO_ID, FILENAME, LOCAL_DIR, IMAGES_DIR_TAR, DS_DIR, IMAGES_DIR, CAPTIONS_CSV, EMBEDDINGS_CSV
+
+def get_dataset_from_huggingface():
+  os.makedirs(LOCAL_DIR, exist_ok=True)
+  os.makedirs(DS_DIR, exist_ok=True)
+  os.makedirs(IMAGES_DIR, exist_ok=True)
+  os.makedirs(CAPTIONS_CSV, exist_ok=True)
+  os.makedirs(EMBEDDINGS_CSV, exist_ok=True)
+
+  if os.listdir(LOCAL_DIR):
+    print(f"✅ {LOCAL_DIR} already contains data. Skipping download and extraction.")
+  else: 
+    print(f"🚧 Downloading 115k compressed images from Laion6.5+ dataset from Hugging Face Hub into {LOCAL_DIR} ...")
+    # Download the model file
+    local_path = hf_hub_download(repo_id=REPO_ID, filename=FILENAME, repo_type="dataset")
+    # Copy to the target directory
+    shutil.copy(local_path, os.path.join(LOCAL_DIR, FILENAME))
+    print(f"✅ 115k compressed images from Laion6.5+ saved to: {LOCAL_DIR}/{FILENAME}")
+
+    print(f"\n🚧 Starting decompression from {LOCAL_DIR}/{FILENAME} into {DS_DIR} ")
+    with tarfile.open(IMAGES_DIR_TAR, 'r:gz') as imgs_tar:
+      imgs_tar.extractall(path=DS_DIR)
+    print(f"✅ Decompression complete.")
+
+    shutil.move(DS_DIR+'Laion6_5_plus_115k/imgs.npy', IMAGES_DIR)
+    print(f"\n✅ Images moved to {IMAGES_DIR} ")
+
+    shutil.move(DS_DIR+'Laion6_5_plus_115k/captions.csv', CAPTIONS_CSV)
+    print(f"\n✅ Captions moved to {CAPTIONS_CSV} ")
+
+    shutil.move(DS_DIR+'Laion6_5_plus_115k/embeddings.npy', EMBEDDINGS_CSV)
+    print(f"\n✅ Embeddings moved to {EMBEDDINGS_CSV} ")
+
+    os.rmdir(DS_DIR+'Laion6_5_plus_115k')
+
+if __name__ == "__main__":
+  get_dataset_from_huggingface()
